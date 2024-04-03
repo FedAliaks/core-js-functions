09541ff1da11f4cad07e125bf18256619afbdd81
Aliaksandr Fedukovich <1632637@mail.ru>
2024-04-03 12:39:03 +0300

    feat: add solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -112,8 +112,16 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cash = {};
+  return (n) => {
+    if (cash[n]) {
+      return cash[n];
+    }
+    const result = func(n);
+    cash[n] = result;
+    return result;
+  };
 }
 
 /**
