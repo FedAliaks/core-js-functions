88a0cdd1b2754169b9050871b16078aa824afdca
Aliaksandr Fedukovich <1632637@mail.ru>
2024-04-03 13:42:40 +0300

    feat: add solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -94,8 +94,12 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  const elements = args.reverse();
+
+  return (n) => {
+    return elements.reduce((acc, item, index) => acc + item * n ** index, 0);
+  };
 }
 
 /**
